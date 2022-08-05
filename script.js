d77c76fccfa440846e4d2e983d3d5f85c22add25
GaronCode <70383307+GaronCode@users.noreply.github.com>
2022-08-05 12:22:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Classic player for Youtube shorts
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  change player to classic in youtube shorts
+// @author       Garon
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let a = (h) => {
+        if(h.includes('/shorts/')) document.location.replace(window.location.href.replace('/shorts/', '/watch?v='))
+    }
+    let href = window.location.href;
+
+    a(href);
+    setInterval(() => {
+        if (href !== window.location.href) {
+            href = window.location.href;
+            a(href);
+        }
+    }, 50)
+})();
