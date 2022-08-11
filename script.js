58694baf9974c6e6d5abe8f9070d636b79990b2e
GaronCode <70383307+GaronCode@users.noreply.github.com>
2022-08-11 12:50:37 +0300

    Update script.js
    fixed bug

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // ==UserScript==
 // @name         Classic player for Youtube shorts
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      1.1
+// @homepageURL  https://github.com/GaronCode/shorts-to-classic
+// @supportURL   https://github.com/GaronCode/shorts-to-classic/issues
+// @updateURL    https://raw.githubusercontent.com/GaronCode/shorts-to-classic/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/GaronCode/shorts-to-classic/main/script.js
 // @description  change player to classic in youtube shorts
 // @author       Garon
 // @match        https://www.youtube.com/*
@@ -13,9 +17,8 @@
 (function() {
 
     'use strict';
-    let a = () => {
-        if(window.location.href.includes('/shorts/')) document.location.replace(window.location.href.replace('/shorts/', '/watch?v='))
-    }
-    a();
-    document.body.addEventListener("yt-navigate-start", ()=>a());
+    let a = () => {if(window.location.href.includes('/shorts/')) document.location.replace(window.location.href.replace('/shorts/', '/watch?v='))}
+    a()
+    window.addEventListener('yt-navigate-start', a)
+
 })();
